7f7db4e3c590faf695ae3766ad74db5df7fb3c34
Zz <267457761+dropout-zzz@users.noreply.github.com>
2026-09-04 14:46:26 +0900

    ok

--- make_tag_db.sh
+++ make_tag_db.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # run in the "Database" repo
 # run ./scripts/init.sh before this for first time dev
+# it generates many files but only "db.html.json" is used by us
+# other files is probably for different type of clients
 node --enable-source-maps tool create-release . ./publish
