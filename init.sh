47ba6261847e880bd2cc6075c634cf0fda225d41
Zz <267457761+dropout-zzz@users.noreply.github.com>
2026-09-04 14:54:01 +0900

    eh

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # for first time development after cloning sources,
 # run this to create node_modules/
+# may need a specific ver of NodeJS to build
+# see package.json for more information
 pnpm install
